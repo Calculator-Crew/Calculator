db138c6ddedd64627299d8ffa09ffbafee2b4d44
brandongossen <brandongossen@yahoo.com>
2017-08-28 11:09:58 -0500

    Add functionality for the 'C' button

--- js/calculator.js
+++ js/calculator.js
@@ -1,17 +1,26 @@
 (function () {
     "use strict";
 
-    var calculator = {};
-    calculator.leftOperand = "";
-    calculator.operator = "";
-    calculator.rightOperand = "";
+    // var calculator = {};
+    // calculator.leftOperand = "";
+    // calculator.operator = "";
+    // calculator.rightOperand = "";
+    var buttonPressed = "";
 
+    var leftOperand = document.getElementById("leftOperand");
     var calculatorButtons = document.getElementsByClassName("calculatorBtn");
 
     for (var i = 0; i < calculatorButtons.length; i++) {
         calculatorButtons[i].addEventListener('click', function (event) {
            event.preventDefault();
-           console.log(this.innerText);
+           buttonPressed = this.innerText;
+           leftOperand.value = leftOperand.value + buttonPressed;
+           switch(buttonPressed) {
+               case "C":
+                   leftOperand.value = "";
+                   break;
+           }
+
         });
     }
 })();
