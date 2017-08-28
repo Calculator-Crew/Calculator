59ea02e575729739d806143c06cae0349350469c
brandongossen <brandongossen@yahoo.com>
2017-08-28 12:45:55 -0500

    Add functionality for entering mathematical expressions into the caluclator\n- add operatorBtn and operandBtn classes to the calculator buttons\n- add logical statements to determine which operand input to place the clicked buttons values in

--- js/calculator.js
+++ js/calculator.js
@@ -8,19 +8,35 @@
     var buttonPressed = "";
 
     var leftOperand = document.getElementById("leftOperand");
+    var operator = document.getElementById("operator");
+    var rightOperand = document.getElementById("rightOperand");
     var calculatorButtons = document.getElementsByClassName("calculatorBtn");
 
     for (var i = 0; i < calculatorButtons.length; i++) {
         calculatorButtons[i].addEventListener('click', function (event) {
-           event.preventDefault();
+            event.preventDefault();
            buttonPressed = this.innerText;
-           leftOperand.value = leftOperand.value + buttonPressed;
+
+           if (isOperatorBtn(this)) {
+               operator.value = buttonPressed;
+           } else if (operator.value.length > 0) {
+               rightOperand.value = rightOperand.value + buttonPressed;
+           } else {
+               leftOperand.value = leftOperand.value + buttonPressed;
+           }
+
            switch(buttonPressed) {
                case "C":
                    leftOperand.value = "";
+                   operator.value = "";
+                   rightOperand.value = "";
                    break;
            }
 
         });
+    }
+
+    function isOperatorBtn (buttonToTest) {
+        return (buttonToTest.className.split(" ").indexOf("operatorBtn") > -1 ? true : false);
     }
 })();
