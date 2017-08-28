008ba0b60753d4b02951620b25023a79d9cc70da
Travis Payne <thpaggie910@gmail.com>
2017-08-28 14:18:01 -0500

    Add 'equals' button functionality to calculate the result of an arithmetic operation:\n- create function to calculate the correct value based off of the operator input value\n- refactor conditional statement into the buttonPressed switch\n-Add error checking to make sure each input field has a value

--- js/calculator.js
+++ js/calculator.js
@@ -14,16 +14,8 @@
 
     for (var i = 0; i < calculatorButtons.length; i++) {
         calculatorButtons[i].addEventListener('click', function (event) {
-            event.preventDefault();
+           event.preventDefault();
            buttonPressed = this.innerText;
-
-           if (isOperatorBtn(this)) {
-               operator.value = buttonPressed;
-           } else if (operator.value.length > 0) {
-               rightOperand.value = rightOperand.value + buttonPressed;
-           } else {
-               leftOperand.value = leftOperand.value + buttonPressed;
-           }
 
            switch(buttonPressed) {
                case "C":
@@ -31,6 +23,23 @@
                    operator.value = "";
                    rightOperand.value = "";
                    break;
+
+               case "=":
+                   if(leftOperand.value.length > 0 && rightOperand.value.length > 0 && operator.value.length > 0) {
+                       leftOperand.value = computeCalculation(leftOperand.value, rightOperand.value, operator.value);
+                       operator.value = "";
+                       rightOperand.value = "";
+                   }
+                   break;
+
+               default:
+                   if (isOperatorBtn(this)) {
+                       operator.value = buttonPressed;
+                   } else if (operator.value.length > 0) {
+                       rightOperand.value = rightOperand.value + buttonPressed;
+                   } else {
+                       leftOperand.value = leftOperand.value + buttonPressed;
+                   }
            }
 
         });
@@ -38,5 +47,18 @@
 
     function isOperatorBtn (buttonToTest) {
         return (buttonToTest.className.split(" ").indexOf("operatorBtn") > -1 ? true : false);
+    }
+
+    function computeCalculation(leftOperand, rightOperand, operator) {
+        switch (operator) {
+            case "+":
+                return Number(leftOperand) + Number(rightOperand);
+            case "-":
+                return Number(leftOperand) - Number(rightOperand);
+            case "*":
+                return Number(leftOperand) * Number(rightOperand);
+            case "/":
+                return Number(leftOperand) / Number(rightOperand);
+        }
     }
 })();
